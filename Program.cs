166f1ab1f259da82745db8de69af02ce8bdd0ee8
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-20 14:03:04 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SpecialNumbersSumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());           
+
+            for (int i = 1; i <= num; i++)
+            {
+                int sumDigits = 0;
+                int iNum = i;
+
+                while(iNum > 0)
+                {
+                    int digit = iNum % 10;
+                    sumDigits += digit;
+                    iNum = iNum / 10;                   
+                }
+
+                if (sumDigits == 5 || sumDigits == 7 || sumDigits == 11)
+                {
+                    Console.WriteLine($"{i} -> True");                    
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }                
+            }
+        }
+    }
+}
